2ead64ea19ffadc71133e005df2b3f9156542bc6
Alexander <98586297+Alexander-Kezik@users.noreply.github.com>
2023-10-12 12:28:05 +0200

    + documentation

--- src/app/lib/utils/stringToGameOfLifeParams.ts
+++ src/app/lib/utils/stringToGameOfLifeParams.ts
@@ -1,3 +1,10 @@
+/**
+ * Converts a string into Game of Life parameters.
+ *
+ * @param arr - A string representing the initial state of the Game of Life.
+ * @param iterations - A string representing the number of iterations for the game.
+ * @returns An object containing the Game of Life parameters.
+ */
 export const stringToGameOfLifeParams = (arr: string, iterations: string) => {
   const rows: string[] = arr.trim().split("\n");
   const initialGameOfLifeState: (0 | 1)[][] = rows.map(row => row.split("").map(
